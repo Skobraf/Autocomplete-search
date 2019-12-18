7b3f2e21e42f85f1359432c3db654e663cef565c
skobraf <skobraf@gmail.com>
2019-12-18 15:28:47 +0000

    filter and display elemnts on userInput

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -21,21 +21,84 @@ class Autocomplete extends Component {
         userInput
         });
     }
+    onClick = (e) => {
+        this.setState({
+          activeOption: 0,
+          filteredOptions: [],
+          showOptions: false,
+          userInput: e.currentTarget.innerText
+        });
+      };
+    onKeyDown = (e) => {
+        const { activeOption, filteredOptions } = this.state;
+        if (e.keyCode === 13) { // ENTER KEY.
+          this.setState({
+            activeOption: 0,
+            showOptions: false,
+            userInput: filteredOptions[activeOption]
+          });
+        } else if (e.keyCode === 38) { // UP ARROW.
+          if (activeOption === 0) {
+            return;
+          }
+          this.setState({ activeOption: activeOption - 1 });
+        } else if (e.keyCode === 40) { // DOWN ARROW.
+          if (activeOption === filteredOptions.length - 1) {
+            return;
+          }
+          this.setState({ activeOption: activeOption + 1 });
+        }
+      };
     render() {
+        const {
+            handleChange,
+            onClick,
+            onKeyDown,
+      
+            state: { activeOption, filteredOptions, showOptions, userInput }
+          } = this;
+          let optionList;
+          if (showOptions && userInput) {
+            if (filteredOptions.length) {
+              optionList = (
+                <ul className="options">
+                  {filteredOptions.map((optionName, index) => {
+                    let className;
+                    if (index === activeOption) {
+                      className = 'option-active';
+                    }
+                    return (
+                      <li className={className} key={optionName} onClick={onClick}>
+                        {optionName}
+                      </li>
+                    );
+                  })}
+                </ul>
+              );
+            } else {
+              optionList = (
+                <div className="no-options">
+                  <em>No Option!</em>
+                </div>
+              );
+            }
+          }
         return (
             <>
                 <div className="search">
                     <input
                         type="text"
                         className="search-box"
-                        onChange={this.handleChange}
-                        
+                        onChange={handleChange}
+                        onKeyDown={onKeyDown}
+                        value={userInput}
                         />
                     <input
                         type="text"
                         className="search-btn"
                         />
                 </div>
+                {optionList}
             </>
         )
     }
